7520a212d1f598bc0c981b842744e1f9ec5112c8
Cem Genar <cemgenar@gmail.com>
2022-10-29 08:35:10 +0300

    Hot Fix
    Fix context error

--- admob_mediation/src/main/java/com/hmscl/huawei/admob_mediation/InterstitialAds/HuaweiInterstitialCustomEventLoader.kt
+++ admob_mediation/src/main/java/com/hmscl/huawei/admob_mediation/InterstitialAds/HuaweiInterstitialCustomEventLoader.kt
@@ -50,13 +50,19 @@ class HuaweiInterstitialCustomEventLoader(
     /** Loads the interstitial ad from the third party ad network.  */
     fun loadAd() {
         Log.d(TAG, "InterstitialEventLoader - loadAd()")
+
+        context = mediationInterstitialAdConfiguration.context
         if(context == null){
+            Log.d(
+                TAG,
+                "BannerEventLoader - loadAd() - Context is null, an activity context is required to show the ad"
+            )
             mediationAdLoadCallback.onFailure(
                 CustomEventError.createCustomEventNoActivityContextError()
             )
             return
         }
-        context = mediationInterstitialAdConfiguration.context
+
 
         // All custom events have a server parameter named "parameter" that returns back the parameter
         // entered into the AdMob UI when defining the custom event.
